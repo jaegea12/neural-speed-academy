bb82025ba029c1fc8161316e9dbc871a0203ff48
api <project_533123_bot_f8362876807b1cad1ea167fb68685d3f@noreply.code.roche.com>
2026-04-02 17:51:48 +0200

    Add Rapid Decision Grid exercise (M4)

--- nsa.py
+++ nsa.py
@@ -21,7 +21,7 @@ from neural_speed_academy.screens.stats_screen import StatsScreen
 from neural_speed_academy.screens.introduction_screen import IntroductionScreen
 from neural_speed_academy.screens.menu_screens import (
     FlashMenuScreen, WordsMenuScreen, EyespanMenuScreen, PrimingMenuScreen,
-    SequenceMemoryMenuScreen,
+    SequenceMemoryMenuScreen, RapidDecisionMenuScreen,
 )
 from neural_speed_academy.screens.paths_screen import PathSelectionScreen, CustomPathsScreen
 from neural_speed_academy.screens.path_session_screen import (
@@ -112,6 +112,9 @@ class NeuralSpeedAcademy:
         nav.register_screen(
             "sequence_memory_menu",
             lambda: SequenceMemoryMenuScreen(nav))
+        nav.register_screen(
+            "rapid_decision_menu",
+            lambda: RapidDecisionMenuScreen(nav))
 
     def _exercise_callbacks(self) -> dict:
         nav = self.navigator
@@ -125,6 +128,7 @@ class NeuralSpeedAcademy:
             "setup_rsvp": lambda: nav.launch_exercise(RsvpExercise),
             "setup_chunking": lambda: nav.launch_exercise(ChunkingExercise),
             "menu_sequence_memory": lambda: nav.navigate_to("sequence_memory_menu"),
+            "menu_rapid_decision": lambda: nav.navigate_to("rapid_decision_menu"),
             "show_paths": lambda: nav.navigate_to("paths"),
             "show_stats": lambda: nav.navigate_to("stats"),
         }
